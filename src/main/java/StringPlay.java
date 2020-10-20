6eb8e71572c2519ea9bdc0815cea27ecfd34d300
Unknown <blossomperez18@gmail.com>
2020-10-20 12:27:15 -0500

    Complete StringUtils practice

--- src/main/java/StringPlay.java
+++ src/main/java/StringPlay.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
 import org.apache.commons.lang3.StringUtils;
+
 public class StringPlay {
     public static void main(String[] args) {
-        System.out.print(capitlizeLetter("b"));
-    }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter something: ");
+        String input = sc.nextLine();
+        System.out.printf("You entered: %s%n", input);
 
-    public static String capitlizeLetter(String letter) {
-        return StringUtils.upperCase(letter);
+        if(!StringUtils.isNumeric(input)) {
+            System.out.printf("\"%s\" is not numeric%n", input);
+        }else{
+            System.out.printf("\"%s\" is numeric%n", input);
+        }
+        System.out.printf("Flipped Case: %s%n", StringUtils.swapCase(input));
+        System.out.printf("Reversed: %s%n", StringUtils.reverse(input));
     }
+    
+    
+   
 }
